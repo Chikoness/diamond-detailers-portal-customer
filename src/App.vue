b7860aaf766a05cc877cbf18f733c0d354954ca7
Charlene A <chichiko90@gmail.com>
2023-07-04 03:13:57 +0800

    Fire weatherDistance API every 5 minutes

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import BreadcrumbsBar from './components/BreadcrumbsBar.vue';
 import { Geolocation } from '@capacitor/geolocation';
+import axios from "axios";
 
 export default {
   components: {
@@ -20,7 +21,38 @@ export default {
   async mounted() {
     const coord = await Geolocation.getCurrentPosition();
 
-    console.log(coord.coords.latitude)
+    this.$nextTick(function () {
+      window.setInterval(() => {
+        if (localStorage.getItem("email") !== undefined) {
+          console.log(coord.coords.latitude)
+          console.log(coord.coords.longitude)
+
+          axios
+            .get(
+              `https://api.openweathermap.org/data/2.5/weather?lat=${coord.coords.latitude}&lon=${coord.coords.longitude}&appid=db4a24c53fcb9e886f91a65e5fd19e9c`
+            ).then((res) => {
+              console.log(res.data.weather[0].id);
+
+              let sendWeatherDistance = {
+                email: "chichiko90@gmail.com",
+                weatherDistance: {
+                  lat: coord.coords.latitude,
+                  long: coord.coords.longitude,
+                  weather: res.data.weather[0].id
+                }
+              }
+
+              axios
+                .post(
+                  process.env.VUE_APP_BACKEND +
+                  "/api/customer/getWeatherDistance",
+                  sendWeatherDistance
+                )
+            })
+        }
+      }, 60000);
+    })
+
   }
 }
 </script>
